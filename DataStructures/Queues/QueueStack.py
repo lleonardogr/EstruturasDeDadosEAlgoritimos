ef475aeace62931b9050eb1df510e9ef9cae4d5c
Leonardo Gasparini Romão <lleonardogr@gmail.com>
2023-04-20 16:36:47 -0300

    Implementando reverter a fila

--- DataStructures/Queues/QueueStack.py
+++ DataStructures/Queues/QueueStack.py
@@ -30,6 +30,14 @@ class Queue:
                 self.outstorage.push(self.instorage.pop())
         return self.outstorage.pop()
     
+    def reverse_queue(queue):
+        stack = Stack()
+        while not queue.is_empty():
+            stack.push(queue.dequeue())
+    
+        while not stack.is_empty():
+            queue.enqueue(stack.pop())
+    
 
 def testQueue():
     # Setup
